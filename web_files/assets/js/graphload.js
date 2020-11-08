72db740680ecac70698bc6812ae9f5203006485f
phantombeast <rajpalpulkit12@gmail.com>
2020-11-08 23:49:14 +0530

    added merge sort

--- web_files/assets/js/graphload.js
+++ web_files/assets/js/graphload.js
@@ -247,12 +247,60 @@ window.onload = function () {
         }
       }
 
+      function merge(left, right) {
+        var result = [],
+            i = 0,
+            j = 0;
+
+        while (i < left.length && j < right.length) {
+
+            // Elements in both arrays are compared against each other.
+            // Whichever element is smaller it's inserted in the results.
+
+            if (left[i] < right[j]) {
+                result.push(left[i++]);       // Each time a value from one array is added, it's 
+            } else {                       // corresponding index variable is incremented.
+                result.push(right[j++]);
+            }
+        }
+
+        // As soon as one of the arrays has been finished, the
+        // remaining values are added to the end of the result array
+
+        mydata2 = result.concat(left.slice(i)).concat(right.slice(j));
+        youpdte(mydata2);
+
+        return mydata2;
+
+    }
+
     async function youpdte(d) {
         await timer(500);
         myChart.data.datasets[0].data = mydata2;
         myChart.update();
     }
     
+    function MergeSort(arr) {
+
+        var len = arr.length,       // number of items in the array 
+            middle,                     // middle of the array
+            left,                       // left side of the array 
+            right;                      // right side of the array 
+
+        // Arrays with 0 or 1 elements don't need sorting
+        if (len < 2) {
+            return arr;
+        }
+
+        middle = Math.floor(len / 2);
+
+
+        left = arr.slice(0, middle);   // left side, from 0 to the middle  
+        right = arr.slice(middle);     // right side, from the middle to the end
+
+        return merge(MergeSort(left), MergeSort(right));
+
+    }
     sortButton.onclick = function () {
 
         sortButton.disabled  = true;
